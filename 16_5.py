377f1cf09cf258dee38d97456a53013f240665b9
Oleg <sagdeev.ol@yandex.ru>
2024-10-24 00:22:57 +0300

    Update 16_5.py

--- 16_5.py
+++ 16_5.py
@@ -1,57 +1,52 @@
-from fastapi import FastAPI, HTTPException, status, Body, Request
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
-from fastapi.templating import Jinja2Templates
-from typing import List
 
-app = FastAPI()
+app = FastAPI(swagger_ui_parameters={"tryItOutEnabled": True})
+templates = Jinja2Templates(directory='templates')
 
-templates = Jinja2Templates(directory="templates")
-
-
+users = []
+#uvicorn Module_16.16_5:app
 class User(BaseModel):
     id: int
     username: str
     age: int
 
-users =[
-    User(id=1, username="UrbanUser", age=24),
-    User(id=2, username="UrbanTest", age=22),
-    User(id=3, username="Capybara", age=60)
-]
 
 @app.get('/')
-async def get_all_users(request: Request) -> HTMLResponse:
+async def get_main_page(request: Request):
     return templates.TemplateResponse('users.html', {'request': request, 'users': users})
 
 
 @app.get('/user/{user_id}')
-async def get_user(request: Request, user_id: int) -> HTMLResponse:
+async def get_users(request: Request, user_id: int) -> HTMLResponse:
     try:
-        return templates.TemplateResponse('users.html', {'request': request, 'user': users[user_id]})
-    except:
-        raise HTTPException(status_code=404, detail="User was not found")
+        return templates.TemplateResponse('users.html', {'request': request, 'user': users[user_id - 1]})
+    except IndexError:
+        raise HTTPException(status_code=404, detail="User not found")
+
 
 @app.post('/user/{username}/{age}')
-async def create_user(username: str, age: int) -> str:
-    user_id = (users[-1].id + 1) if users else 1
-    new_user = User(id=user_id, username=username, age=age)
+async def post_user(username: str, age: int):
+    new_user = User(id=len(users) + 1, username=username, age=age)
     users.append(new_user)
-    return f"User {new_user} is registered"
+    return new_user
+
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: str, username: str, age: int) -> str:
-    for user in users:
-        if user.id == user_id:
-            user.username = username
-            user.age = age
-            return user
-    raise HTTPException(status_code=404, detail="User was not found")
+async def update_user(user_id: int, username: str, age: int):
+    try:
+        users[user_id - 1] = User(id=user_id, username=username, age=age)
+        return users[user_id - 1]
+    except IndexError:
+        raise HTTPException(status_code=404, detail='User was not found')
+
 
 @app.delete('/user/{user_id}')
-async def delete_user(user_id: str) -> str:
-    for index, user in enumerate(users):
-        if user.id == user_id:
-            deleted_user = users.pop(index)
-            return deleted_user
-    raise HTTPException(status_code=404, detail="User was not found")
+async def delete_user(user_id: int):
+    if user_id == users[user_id - 1].id:
+        return users.pop(user_id - 1)
+    else:
+        raise HTTPException(status_code=404, detail='User was not found')
+
